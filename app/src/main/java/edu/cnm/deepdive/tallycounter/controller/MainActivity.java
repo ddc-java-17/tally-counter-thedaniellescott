c360fadb1f9c7b9c7ef181d27049c40e3e604195
DanielleScott <dmyers28@cnm.edu>
2024-02-05 15:31:07 -0700

    Moved state to viewmodel.

--- app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
@@ -2,23 +2,19 @@ package edu.cnm.deepdive.tallycounter.controller;
 
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import edu.cnm.deepdive.tallycounter.R;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.tallycounter.databinding.ActivityMainBinding;
-import org.jetbrains.annotations.NotNull;
+import edu.cnm.deepdive.tallycounter.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
   private static final String TAG = MainActivity.class.getSimpleName();
 
-  private int counter;
-
   private ActivityMainBinding binding;
+  private MainViewModel viewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,70 +22,23 @@ public class MainActivity extends AppCompatActivity {
     Log.d(TAG, "MainActivity::onCreate");
     binding = ActivityMainBinding.inflate(getLayoutInflater());
     setContentView(binding.getRoot());
-    if (savedInstanceState != null) {
-      setCounter(savedInstanceState.getInt("counter", 0));
-    } else {
-      setCounter(0);
-    }
-    Button increment = findViewById(R.id.increment);
-//    increment.setOnClickListener(this::handleIncrement);
-  }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-    Log.d(TAG, "MainActivity::onRestart");
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-    Log.d(TAG, "MainActivity::onStart");
-  }
-
-  @Override
-  protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-    super.onRestoreInstanceState(savedInstanceState);
-    Log.d(TAG, "MainActivity::onRestoreInstanceState");
-  }
-
-  @Override
-  protected void onResume() {
-    super.onResume();
-    Log.d(TAG, "MainActivity::onResume");
-  }
-
-  @Override
-  protected void onPause() {
-    Log.d(TAG, "MainActivity::onPause");
-    super.onPause();
-  }
-
-  @Override
-  protected void onStop() {
-    Log.d(TAG, "MainActivity::onStop");
-    super.onStop();
-  }
-
-  @Override
-  protected void onSaveInstanceState(@NonNull Bundle outState) {
-    super.onSaveInstanceState(outState);
-    Log.d(TAG, "MainActivity::onSaveInstanceState");
-    outState.putInt("counter", counter);
-  }
-
-  @Override
-  protected void onDestroy() {
-    Log.d(TAG, "MainActivity::onDestroy");
-    super.onDestroy();
+    viewModel = new ViewModelProvider(this)
+        .get(MainViewModel.class);
+    viewModel
+        .getCounter()
+        .observe(this, new Observer<Integer>() {
+          @Override
+          public void onChanged(Integer value) {
+            setTallyDisplay(value);
+          }
+        });
   }
 
   public void handleIncrement(View v) {
-    setCounter(counter + 1);
+    viewModel.incrementCounterValue();
   }
 
-  private void setCounter(int counter) {
-    this.counter = counter;
+  private void setTallyDisplay(int counter) {
     binding.tally.setText(String.valueOf(counter));
   }
 
---
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/viewmodel/MainViewModel.java
@@ -0,0 +1,26 @@
+package edu.cnm.deepdive.tallycounter.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+  private final MutableLiveData<Integer> counter;
+
+  public MainViewModel() {
+    counter = new MutableLiveData<>(0);
+  }
+
+  public LiveData<Integer> getCounter() {
+    return counter;
+  }
+
+  public void setCounterValue(int value) {
+    counter.setValue(value);
+  }
+
+  public void incrementCounterValue() {
+    counter.setValue(counter.getValue() + 1);
+  }
+}

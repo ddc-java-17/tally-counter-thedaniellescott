241e2c76e54adc0888b3816363d670727e74cbcb
DanielleScott <dmyers28@cnm.edu>
2024-02-05 14:01:01 -0700

    Moved anonymous class to inline anonymous class

--- app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/controller/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
+
     Log.d(TAG, "MainActivity::onCreate");
     setContentView(R.layout.activity_main);
     tally = findViewById(R.id.tally);
@@ -31,13 +31,12 @@ public class MainActivity extends AppCompatActivity {
       setCounter(0);
     }
     Button increment = findViewById(R.id.increment);
-    OnClickListener incrementListener = new OnClickListener() {
+    increment.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         setCounter(counter + 1);
       }
-    };
-    increment.setOnClickListener(incrementListener);
+    });
   }
 
   @Override

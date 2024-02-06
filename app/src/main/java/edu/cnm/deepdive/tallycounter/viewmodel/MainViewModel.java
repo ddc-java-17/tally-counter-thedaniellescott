2220c9273d19c6727ab454edbfbaf530927c2387
DanielleScott <dmyers28@cnm.edu>
2024-02-05 17:06:59 -0700

    Made a new layout for landscape mode... among other things.

--- app/src/main/java/edu/cnm/deepdive/tallycounter/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/viewmodel/MainViewModel.java
@@ -3,24 +3,43 @@ package edu.cnm.deepdive.tallycounter.viewmodel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import edu.cnm.deepdive.tallycounter.Subtally;
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainViewModel extends ViewModel {
 
   private final MutableLiveData<Integer> counter;
+  private final MutableLiveData<List<Subtally>> subtallies;
 
   public MainViewModel() {
     counter = new MutableLiveData<>(0);
+    subtallies = new MutableLiveData<>(new LinkedList<>());
   }
 
   public LiveData<Integer> getCounter() {
     return counter;
   }
 
-  public void setCounterValue(int value) {
-    counter.setValue(value);
+  public LiveData<List<Subtally>> getSubtallies() {
+    return subtallies;
+  }
+
+  public void resetCounterValue() {
+    counter.setValue(0);
   }
 
   public void incrementCounterValue() {
     counter.setValue(counter.getValue() + 1);
   }
+
+  public void captureSubtally() {
+    //noinspection DataFlowIssue
+    Subtally subtally = new Subtally(Instant.now(), counter.getValue());
+    subtallies.getValue().add(subtally);
+    subtallies.setValue(subtallies.getValue());
+    resetCounterValue();
+  }
+
 }

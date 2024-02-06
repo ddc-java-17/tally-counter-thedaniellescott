9813f33776f1f88cf410c72a44103c620a795c69
DanielleScott <dmyers28@cnm.edu>
2024-02-06 10:56:01 -0700

    implemented total, completed adapter for list of subtallies

--- app/src/main/java/edu/cnm/deepdive/tallycounter/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/tallycounter/viewmodel/MainViewModel.java
@@ -2,20 +2,28 @@ package edu.cnm.deepdive.tallycounter.viewmodel;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 import edu.cnm.deepdive.tallycounter.Subtally;
 import java.time.Instant;
 import java.util.LinkedList;
 import java.util.List;
+import kotlin.jvm.functions.Function1;
 
 public class MainViewModel extends ViewModel {
 
   private final MutableLiveData<Integer> counter;
   private final MutableLiveData<List<Subtally>> subtallies;
+  private final LiveData<Integer> total;
 
   public MainViewModel() {
     counter = new MutableLiveData<>(0);
     subtallies = new MutableLiveData<>(new LinkedList<>());
+    total = Transformations.map(subtallies, subtallies ->
+        subtallies
+            .stream()
+            .mapToInt(Subtally::value)
+            .sum());
   }
 
   public LiveData<Integer> getCounter() {
@@ -24,6 +32,10 @@ public class MainViewModel extends ViewModel {
 
   public LiveData<List<Subtally>> getSubtallies() {
     return subtallies;
+  }
+
+  public LiveData<Integer> getTotal() {
+    return total;
   }
 
   public void resetCounterValue() {
